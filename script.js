230cd8ba0af810a15572ce07492c842ee00ce8f0
lilsson <pirckhelavalile@gmail.com>
2021-11-22 22:56:58 +0300

    add index

--- script.js
+++ script.js
@@ -1,42 +1,60 @@
 let mainWraper = document.getElementById('post-block');
-
+let overlay = document.getElementById('overlay');
+let close = document.getElementById('close');
 
 
 function ajax() {
-    let requist = new XMLHttpRequest ();
+    let requist = new XMLHttpRequest();
     requist.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    
 
     requist.addEventListener('load', function() {
-       let data = JSON.parse(requist.responseText);
-       
-       data.forEach(element => {
-           console.log(element;)
-           
-       });
+        let data = JSON.parse(requist.responseText); 
+
+
+        data.forEach(element => {
+            createPost(element);
+        });
+
+        console.log(data);
     });
 
     requist.send();
-
 }
-   
    function createPost(item) {
+
     let divWraper = document.createElement('div');
     divWraper.classList.add('posts');
+    divWraper.setAttribute('data-id', item.id);
 
     let h1 = document.createElement('h1');
-    h1.innerHTML = item.id;
+    h1.innerText = item.id;
 
     let text = document.createElement('div');
     text.classList.add('title');
-    text.innerHTML = item.title; 
+    text.innerText = item.title; 
 
-    divWraper.appendChild()
+    divWraper.appendChild(h1);
+    divWraper.appendChild(text);
 
+    divWraper.addEventListener('click', function(event) {
+        let id = event.target.getAttribute('data-id');
+        openOverlay(id);
+      });
+ 
 
+    mainWraper.appendChild(divWraper);
 
+    console.log(divWraper);
+}
+ 
+ function openOverlay (id) {
+     overlay.classList.add('active');
+     console.log(id);
+ }
    
-   }
-
-   
+ close.addEventListener('click', function(){
+    overlay.classList.remove('active');
+})
 
     ajax();
